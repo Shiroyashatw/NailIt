86a16647782a9661ca793c63916a69d78d80aa1b
Sodaapple <sodaapple7@gmail.com>
2023-01-14 19:16:01 +0800

    丹丹：Backstage靜態畫面終於好了，我哭。

---
+++ Project/NailIt/NailIt/Controllers/TanTanControllers/BackstageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NailIt.Controllers.TanTanControllers
+{
+    public class BackstageController : Controller
+    {
+        public IActionResult Backstage()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
