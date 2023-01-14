aa68bf162fa842c9262441c0a0020dfb39e8f981
Sodaapple <sodaapple7@gmail.com>
2023-01-14 20:58:51 +0800

    丹:試圖呼叫出SQLLIST

--- Project/NailIt/NailIt/Controllers/TanTanControllers/BackstageController.cs
+++ Project/NailIt/NailIt/Controllers/TanTanControllers/BackstageController.cs
@@ -4,11 +4,6 @@ namespace NailIt.Controllers.TanTanControllers
 {
     public class BackstageController : Controller
     {
-        public IActionResult Backstage()
-        {
-            return View();
-        }
-
         public IActionResult Index()
         {
             return View();
