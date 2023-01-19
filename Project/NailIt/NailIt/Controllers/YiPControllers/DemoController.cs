b1bc928b8d1f9801f387a7bdbf0a91bcf220d25b
lucky <lucky121415@gmail.com>
2023-01-20 02:34:32 +0800

    我交

--- Project/NailIt/NailIt/Controllers/YiPControllers/DemoController.cs
+++ Project/NailIt/NailIt/Controllers/YiPControllers/DemoController.cs
@@ -21,16 +21,11 @@ namespace NailIt.Controllers.YiPControllers
         [HttpGet("{id}")]
         public async Task<ActionResult<IEnumerable<dynamic>>> GetThisDemoTable(int id)
         {
-            var query = from User in Context.DemoSetTables
-                        join Demo in Context.DemoTables
-                        on User.DemoSetId equals Demo.DemoSetId
-                        where User.ManicuristId == id
-                        select new
-                        {
-                            DemoSetId = Demo.DemoSetId,
-                            DemoSetName = User.DemoSetName,
-                            DemoPic = Demo.DemoPic
-                        };
+            var query = from User in Context.DemoTables
+                            //join Demo in Context.DemoTables
+                            //on User.DemoSetId equals Demo.DemoSetId
+                        where User.DemoSetId == id
+                        select User;
             //select User;
             return await query.ToListAsync();
         }
@@ -38,7 +33,7 @@ namespace NailIt.Controllers.YiPControllers
         [HttpPost]
         public async Task<ActionResult<DemoSetTable>> PostDemoTable(List<DemoTable> DemoTable)
         {
-            List<DemoTable> DemoTables = new List<DemoTable>();
+            //List<DemoTable> DemoTables = new List<DemoTable>();
             //DemoTables.AddRange(DemoTable);
             foreach (var DemoS in DemoTable)
             {
@@ -50,5 +45,18 @@ namespace NailIt.Controllers.YiPControllers
             //return CreatedAtAction("GetDemotTable", new { id = DemoTable.DemoSetId }, DemoTable);
             return Ok();
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDemoTable(int id)
+        {
+            var DemoTable = await Context.DemoTables.FindAsync(id);
+            //var DeleteCertainDemoSet = (from o in Context.DemoSetTables
+            //                      where o.DemoSetId == id
+            //                      select o).FirstOrDefault();
+            Context.DemoTables.Remove(DemoTable);
+            await Context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
