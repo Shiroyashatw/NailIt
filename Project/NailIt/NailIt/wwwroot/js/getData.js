ea21db0cb218de5a78c567a69b1a8420908938b2
Shiroyashatw <yungmin1132@gmail.com>
2023-02-01 10:21:00 +0800

    商品變更

--- Project/NailIt/NailIt/wwwroot/js/getData.js
+++ Project/NailIt/NailIt/wwwroot/js/getData.js
@@ -39,11 +39,14 @@ let ritem
 let ritemName
 let rdep
 
+
+
 // 讀取基本資料 設計師資料 demoset資料 demo資料 再顯示在畫面上
 // 一進畫面就進行讀取
 function getbasicinfo() {
     // 一進畫面就帶入商品ID 進檢舉商品
     $('input[name="ReportItem"]').val(DSETID);
+
     $.ajax({
         url: "https://localhost:44308/api/product/" + DSETID,
         method: 'GET',
@@ -135,6 +138,10 @@ function getReserveDate() {
     // onsole.log(typeof(id)); // string
     $('#reservebtn, .lastMonth, .nextMonth,#Sendbtn').on('click', function () {
         // 讀取的設計師ID傳回設定
+        $('input[name="MemberId"]').val();
+        if ($('input[name="MemberId"]').val() == '') {
+            alert("請先登入會員再進行預約")
+        }
         MID = $("input[name='ManicuristId']").attr('value')
         $.ajax({
             url: `https://localhost:44308/api/product/${MID}/reserve`,
@@ -308,7 +315,7 @@ function postOrder() {
 
 // 讀取設計師資料
 function getManicuristData() {
-    MID = url.searchParams.get('MID');
+    MID = url.searchParams.get('id');
     $.ajax({
         url: `https://localhost:44308/api/product/MID/${MID}`,
         method: "GET",
@@ -503,7 +510,7 @@ function postCash() {
     let MerchantTradeDate = $('input[name="MerchantTradeDate"]').val();
 
     //console.log(Tzdatetime)
-     
+
     $('input[name="MerchantTradeNo"]').val(`Nailit${Tzymd + Tztime}`);
     let MerchantTradeNo = $('input[name="MerchantTradeNo"]').val();
 
@@ -514,7 +521,7 @@ function postCash() {
     // let OrderResultURL = $('input[name="OrderResultURL"]').val();
     $('input[name="TotalAmount"]').val(rdep);
     let TotalAmount = $('input[name="TotalAmount"]').val();
-    
+
     let TradeDesc = $('input[name="TradeDesc"]').val();
 
     // CheckMacValue 檢查碼生成 上面參數都正確會傳放入 val()進去
@@ -524,7 +531,7 @@ function postCash() {
     y = y.replace("%20", "+")
     console.log(y)
     y = y.toLowerCase();
-    
+
     var hash = CryptoJS.SHA256(y).toString();
 
     hash = hash.toUpperCase()
@@ -559,5 +566,6 @@ function postCash() {
     // let z = "HashKey%3dpwFHCqoQZGmho4w6%26ChoosePayment%3dCredit%26EncryptType%3d1%26ItemName%3d%e7%b9%bd%e7%b4%9b%26MerchantID%3d3002607%26MerchantTradeDate%3d2023%2f01%2f27+11%3a41%3a12%26MerchantTradeNo%3dDX20230127114112152a%26PaymentType%3daio%26ReturnURL%3dhttps%3a%2f%2fhoyo.idv.tw%2f%3fa%3dTools%2fEcPay%26b%3dReturnURL%26TotalAmount%3d1000%26TradeDesc%3d%e7%be%8e%e7%94%b2%26HashIV%3dEkRm7iFT261dpevs";
     // %20 應該轉換成 +
 }
+
 
 
