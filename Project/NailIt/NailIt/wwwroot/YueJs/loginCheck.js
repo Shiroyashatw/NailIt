f4de9ea554bbeeb85b3ea919e9116e71db4d451e
etai183216@gmail.com <etai183216@gmail.com>
2023-02-04 12:59:40 +0800

    ssdsd

--- Project/NailIt/NailIt/wwwroot/YueJs/loginCheck.js
+++ Project/NailIt/NailIt/wwwroot/YueJs/loginCheck.js
@@ -18,9 +18,27 @@ async function YueloginCheck() {
 		nowNickName = "";
 	}
 	else {
-		JSON.parse(myresult)[0]
+		daMember=JSON.parse(myresult)[0]
 		nowMember = daMember.memberId;
 		nowAccount = daMember.memberAccount;
 		nowNickName = daMember.memberNickname;
 	}
+	headChange();
 }
+
+function headChange() {
+	if (nowMember != -1) {
+		huiORreg.innerText = "會員中心";
+		huiORreg.href = "https://localhost:44308/tedLb/tedmember.html#";
+		loginORout.innerText = "登出";
+		loginORout.addEventListener("click", YueLogout, true);
+	}
+	else {
+		huiORreg.innerText = "註冊";
+		huiORreg.href = "https://localhost:44308/YueRegister.html";
+		loginORout.innerText = "登入";
+		loginORout.href = "https://localhost:44308/Yuelogin.html";
+	}
+}
+
+function 
