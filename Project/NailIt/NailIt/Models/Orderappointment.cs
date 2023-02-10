737b022be601362095dba16529cc7bb041956d95
ASUS TUF <ASUS TUF@DESKTOP-4VI1JLT>
2023-02-10 19:21:30 +0800

    000

--- Project/NailIt/NailIt/Models/Orderappointment.cs
+++ Project/NailIt/NailIt/Models/Orderappointment.cs
@@ -30,7 +30,7 @@ namespace NailIt.Models
         public int MemberId { get; set; }
         public int ManicuristId { get; set; }
         public string MemberName { get; set; }
-        public int DemoSetId { get; set; }
+        public int? DemoSetId { get; set; }
         public bool OrderType { get; set; }
     }
 }
